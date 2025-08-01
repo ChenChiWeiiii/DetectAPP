db6c4b516fba564df6eac6d343590fc3cb020901
caixinhui <hsinhui1128@gmail.com>
2025-08-01 20:05:11 +0800

    雲端IP設置

--- app/src/main/java/com/example/detect/RetrofitClient.java
+++ app/src/main/java/com/example/detect/RetrofitClient.java
@@ -5,7 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://10.22.148.202:3000/";  //API address
+    private static final String BASE_URL = "https://detectapp-backend.onrender.com/"; // Render產生的雲端網址
+    //private static final String BASE_URL = "http://10.22.148.202:3000/";  //API address
     //private static final String BASE_URL = "http://10.0.2.2:3000/";
     private static Retrofit retrofit;
 
