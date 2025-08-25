8c2949dc2865e2ff2dc66c7efc1c3312fa210823
Minnie <peih.hung@gmail.com>
2025-08-25 15:46:35 +0800

    shock and model

--- app/src/main/java/com/example/detect/MainActivity.java
+++ app/src/main/java/com/example/detect/MainActivity.java
@@ -44,9 +44,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import com.example.detect.model.ReminderRequest;
 import com.example.detect.model.SensitivityRequest;
-import com.google.common.util.concurrent.ListenableFuture;
 
 import java.nio.ByteBuffer;
 import java.io.IOException;
@@ -96,7 +96,10 @@ import java.util.UUID;
 import androidx.camera.camera2.interop.Camera2CameraInfo;
 import android.hardware.camera2.CameraCharacteristics;
 import android.util.SizeF;
+import androidx.annotation.OptIn;
+import androidx.camera.camera2.interop.ExperimentalCamera2Interop;
 
+@OptIn(markerClass = ExperimentalCamera2Interop.class)
 public class MainActivity extends AppCompatActivity {
     private PreviewView previewView;
     private OverlayView overlayView;
@@ -310,8 +313,6 @@ public class MainActivity extends AppCompatActivity {
                     Log.w("MiBand", "找不到震動服務");
                 }
 
-                // 🔽 額外呼叫你自訂的震動方法（可選）
-                triggerMiBandVibration();
             }
         });
 
@@ -620,7 +621,8 @@ public class MainActivity extends AppCompatActivity {
 
             ApiService apiService = RetrofitClient.getInstance().create(ApiService.class);
             SensitivityRequest sensitivityRequest = new SensitivityRequest(userId, sensitivityLevel);
-            ReminderRequest reminderRequest = new ReminderRequest(userId,
+            ReminderRequest reminderRequest = new ReminderRequest(
+                    userId,
                     isVoiceEnabled ? 1 : 0,
                     isVibrationEnabled ? 1 : 0);
 
@@ -777,20 +779,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void triggerVibrationOnce(String tag) {
+        if(!isVibrationEnabled) return;
         long now = System.currentTimeMillis();
         if (now - lastVibrationTime < REMINDER_COOLDOWN_MS) return;
 
-        if (isVibrationEnabled) {
-            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
-            } else {
-                vibrator.vibrate(500);
-            }
-            triggerMiBandVibration();
-            lastVibrationTime = now;
-            Log.d("提醒", "觸發提醒: " + tag);
-        }
+        triggerMiBandVibration();
+        lastVibrationTime = now;
+        Log.d("提醒", "觸發提醒: " + tag);
     }
 
     private void speakOnce(String message) {
